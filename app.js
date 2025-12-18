6944f6d68bf0fe13d6f2c7616f03eeb7364c89fe
AlexandreOliveira257 <alexandrept59@gmail.com>
2025-12-18 09:29:25 +0000

    Link para o register

--- app.js
+++ app.js
@@ -18,9 +18,6 @@ app.get('/dashboard', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'dashboard/dashboard.html')); 
 });
 
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'authentication/login.html'));
-});
 // Rota para Registo
 app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'authentication/register.html'));
